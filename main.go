7765df6f92a6d3b8038969a93cd8177951845c55
ZvonimirSkorin <skorinzvonimir0@gmail.com>
2025-04-06 15:48:52 +0200

    Refactor

--- main.go
+++ main.go
@@ -5,21 +5,35 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 )
 
 
 
 func main(){
-	init:=flag.String("init", "init", "init",)
+	command:=flag.String("command", "command", "command",)
 	run:=flag.String("run", "init", "init",)
 	flag.Parse();
 	fmt.Println(*run)
-	if(*init == "start"){
+	switch *command {
+	case "command":
+		fmt.Println("Check for help with -command=help")
+	case "start":
 		os.MkdirAll("mongo-migrations", os.ModePerm)
 		os.Create("mongo-migrations/main.go")
-	}
-	 if(*run == "main"){
+	case "main":
 		exec.Command("go run mongo-migrations/main.go")
-	 }
+	case "create-migration-file":
+		//create file with current date including seconds
+		fileName:="mongo-migrations/"+time.Now().Format("2006-01-02-15-04-05")+".go"
+		exec.Command("go run " + fileName)
+	case "help":
+		fmt.Println("To create migration folder run -command=start")
+		fmt.Println("To create default file run -command=main")
+		fmt.Println("To create migration file run -command=create-migration-file")
+	default:
+		fmt.Println("Unknown command")
+		
+	}
 	
 }
